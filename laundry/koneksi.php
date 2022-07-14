ee2e0175551e8598a5690aa418751fbe53d1565a
Tonoooo <92434748+Tonoooo@users.noreply.github.com>
2022-07-14 16:37:06 +0700

    Delete laundry directory

--- laundry/koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php 
-// nama host, username, password dan nama database
-$koneksi = mysqli_connect("localhost","root","","loundry");
-// Periksa koneksi
-if (mysqli_connect_errno()) {
-	// code...
-	echo "Koneksi database gagal : " . mysqli_connect_error();
-    }
-?>
